d3f7245720ed020838d8611afe91efda7bcf2d34
Gaurav Rawat <gauravrawat02@hotmail.com>
2018-06-15 11:55:47 +0530

    Routing
    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { StudentComponent } from './Student/student.component';
 import { ProjectGridComponent } from './Student/projectgrid.component';
 import { HttpHelper } from './HttpHelper/httphelper.service';
 import { HttpModule } from '@angular/http';
 import { SortPipes } from './Helpers/sortpipe.component';
 import { SearchPipe } from './Helpers/search.component';
+import { RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
+    LayoutComponent,   
+     HomeComponent,
     StudentComponent,
     ProjectGridComponent,
     SortPipes,
-    SearchPipe
+    SearchPipe,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: 'Home',
+          component: HomeComponent
+        },
+        {
+          path: 'Registeration',
+          component: StudentComponent
+        }
+      ]
+    )
   ],
   providers: [],
-  bootstrap: [StudentComponent]
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }

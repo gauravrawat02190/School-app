1c287584c501a8eb64666b74d56accab04fb93d5
Gaurav Rawat <gauravrawat02@hotmail.com>
2018-07-20 21:56:17 +0530

    routing

---
+++ src/app/AppModule/routing-module/routing-module.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Route } from '../../../../node_modules/@angular/router';
+import { StudentComponent } from '../../Student/student.component';
+import { HomeComponent } from '../../home/home.component';
+import { WebTestComponentComponent } from '../../web-test-component/web-test-component.component';
+
+const routes :Route[]=[
+  {
+    path: 'Home',
+    component: HomeComponent    
+  },
+  {
+    path: 'Registeration',
+    component: StudentComponent
+  },
+  {
+    path:'WebTest',
+    component:WebTestComponentComponent
+  },
+  
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule],
+  declarations: []
+})
+
+
+export class RoutingModule {
+
+
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { StudentComponent } from './Student/student.component';
 import { ProjectGridComponent } from './Student/projectgrid.component';
-import { HttpHelper } from './HttpHelper/httphelper.service';
 import { HttpModule } from '@angular/http';
 import { SortPipes } from './Helpers/sortpipe.component';
 import { SearchPipe } from './Helpers/search.component';
-import { RouterModule } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { WebTestComponentComponent } from './web-test-component/web-test-component.component';
+import { RoutingModule } from './AppModule/routing-module/routing-module.module';
 
 @NgModule({
   declarations: [
     LayoutComponent,   
      HomeComponent,
     StudentComponent,
+    WebTestComponentComponent,
     ProjectGridComponent,
     SortPipes,
-    SearchPipe,
-    WebTestComponentComponent,
+    SearchPipe
+   
   ],
   imports: [
     BrowserModule,
+    RoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule,
-    RouterModule.forRoot(
-      [
-        {
-          path: 'Home',
-          component: HomeComponent
-        },
-        {
-          path: 'Registeration',
-          component: StudentComponent
-        },
-        {
-          path:'WebTest',
-          component:WebTestComponentComponent
-        }
-      ]
-    )
-  ],
+        HttpModule
+  ],   
   providers: [],
   bootstrap: [LayoutComponent]
 })
